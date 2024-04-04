e32f90d10ebc1ad3c2afde6acd69e8cb037da968
Rodrigo de Sousa <rodrigodiasdesousa@gmail.com>
2024-04-05 00:28:19 +0100

    index.php started, main navigation bar donish

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<head>
+    <title>GrooveSwap</title>
+    <meta charset="UTF-8">
+    <link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <h1>
+        <ul>
+        <li><div class="logo">
+        <a href="index.php">GrooveSwap</a>
+        </div></li>
+        <li><div class="search">
+            <input type="text" placeholder="Search artists, albums and more...">
+        </div></li>
+        <li><nav id="topics">
+            <ul>
+                <li><a href="pages/new.php">New</a></li>
+                <li><a href="pages/hot.php">Hot</a></li>
+            </ul>
+        </nav></li>
+        <!-- idea is to change to a profile icon when loged in -->
+        <li><div class="login"> 
+            
+            <a href="pages/login.php">Login</a>
+            <a href="pages/register.php">Register</a>
+            
+        </div></li>
+        </ul>
+        <h1>
+    </header>
+    <main>    
+        <section id="publications">
+            <p> content </p>
+        </section>
+    </main>
+    <footer>
+    <p>&copy; 2024  GrooveSwap</p>
+    </footer>
+</body>
