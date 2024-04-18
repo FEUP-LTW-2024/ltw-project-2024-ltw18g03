d2f2d790928414ffd088521886992179f23136be
Rodrigo de Sousa <rodrigodiasdesousa@gmail.com>
2024-04-18 01:39:56 +0100

    bunch of populates

--- db/create.sql
+++ db/create.sql
@@ -8,22 +8,22 @@ DROP TABLE IF EXISTS Review;
 --Table created for every user that is registered in
 CREATE TABLE User
 {
-    ID INT PRIMARY KEY,
+    ID INT PRIMARY KEY AUTOINCREMENT,
     firstName VARCHAR(30) NOT NULL,
     lastName VARCHAR(30) NOT NULL,
-    profilePicture INT REFERENCES ProfilePicture(ID),
+    profilePicture INT REFERENCES ProfilePicture(ID) DEFAULT 1,
     city VARCHAR(30) NOT NULL,
     postalCode CHAR(8) NOT NULL,
     phone CHAR(9) NOT NULL,
     email VARCHAR(30) NOT NULL,
     passwordHash VARCHAR(30) NOT NULL,
-    isAdmin BOOLEAN NOT NULL
+    isAdmin BOOLEAN NOT NULL DEFAULT FALSE,
     joinDate DATE NOT NULL DEFAULT CURRENT_DATE
 };
 -- Table created for every album on the db, not necessarily on sale
 CREATE TABLE Album
 {
-    ID INT PRIMARY KEY,
+    ID INT PRIMARY KEY AUTOINCREMENT,
     title VARCHAR(30) NOT NULL, --album name
     artist VARCHAR(30) NOT NULL, --album artist
     songs TEXT NOT NULL, --all songs, comma-seperated-values
@@ -37,30 +37,31 @@ CREATE TABLE Album
 -- Table created for each item to be sold
 CREATE TABLE Item
 {
-    ID INT PRIMARY KEY,
+    ID INT PRIMARY KEY AUTOINCREMENT,
     title VARCHAR(64) NOT NULL
     descriptionOfItem TEXT DEFAULT 'no description was provided by the seller',
     price FLOAT NOT NULL,
     condition VARCHAR(40),
     listDate DATE NOT NULL DEFAULT CURRENT_DATE,
     seller INT REFERENCES User(ID)
+    album INT REFERENCES Album(ID)
 };
 -- Table created for default profile pictures for Users
 CREATE TABLE ProfilePicture
 {
-    ID INT PRIMARY KEY,
+    ID INT PRIMARY KEY AUTOINCREMENT,
     url VARCHAR(255) NOT NULL
 };
 -- Table created for each picture for each item
 CREATE TABLE Image
 {
-    ID INT PRIMARY KEY,
+    ID INT PRIMARY KEY AUTOINCREMENT,
     itemID INT REFERENCES Item(ID) ON DELETE CASCADE,
     url VARCHAR(255) NOT NULL
 };
 
 CREATE TABLE Review {
-    ID INT PRIMARY KEY,
+    ID INT PRIMARY KEY AUTOINCREMENT,
     itemID INT REFERENCES Item(ID) NOT NULL,
     userID INT REFERENCES User(ID) NOT NULL,
     feedback TEXT NOT NULL,
---
+++ db/users.sql
@@ -0,0 +1,7 @@
+INSERT INTO User (firstName, lastName, profilePicture, city, postalCode, phone, email, passwordHash, isAdmin) 
+VALUES 
+('John', 'Doe', 1, 'New York', '1000-101', '123456789', 'john@example.com', 'hashed_password'),
+('Jane', 'Smith', 2, 'Los Angeles', '9000-111', '987654321', 'jane@example.com', 'hashed_password'),
+('Admin', 'Adminson', 1, 'Admin City', '1234-345', '111222333', 'admin@example.com', 'admin_password_hash', TRUE);
+('Gonçalo', 'Sousa', ...)
+('Rodrigo', 'de Sousa', 5, 'Porto', '4250-453', '966110454', 'rodrigodiasdesousa@gmail.com', 'what_imnot_doingTHAT', TRUE)
